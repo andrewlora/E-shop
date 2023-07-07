51b259709f419d2ba7dfeff7784bdb766f894f7f
Andres Lora <jalc.andres.lora@gmail.com>
2023-07-07 12:38:23 -0700

    added Login and Sign up components/pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { LoginPage, SignupPage } from "./routes.js";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ frontend/src/routes.js
@@ -0,0 +1,4 @@
+import SignupPage from "../src/pages/SignupPage";
+import LoginPage from "./pages/Login";
+
+export { LoginPage, SignupPage };
