63cd25dde1fe4ebcbdfa1bf880fdb1e7e95a2576
Andres Lora <jalc.andres.lora@gmail.com>
2023-07-10 16:12:30 -0700

    added product and wishlist entities to redux

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,13 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cartReducer } from "./reducers/cart";
 import { eventReducer } from "./reducers/event";
+import { productReducer } from "./reducers/product";
 import { userReducer } from "./reducers/user";
+import { wishlistReducer } from "./reducers/wishlist";
 
 const Store = configureStore({
   reducer: {
     user: userReducer,
     events: eventReducer,
     cart: cartReducer,
+    products: productReducer,
+    wishlist: wishlistReducer,
   },
 });
 
