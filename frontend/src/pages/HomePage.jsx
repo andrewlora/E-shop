26838c86683b91ee4ffd6108a214266174e1c654
Andres Lora <jalc.andres.lora@gmail.com>
2023-07-10 15:33:56 -0700

    added popular events into homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Events from "../components/Events/Events";
 import Header from "../components/Layout/Header";
 import Categories from "../components/Route/Categories/Categories";
 import Hero from "../components/Route/Hero/Hero";
@@ -9,6 +10,7 @@ const HomePage = () => {
       <Header activeHeading={1} />
       <Hero />
       <Categories />
+      <Events />
     </div>
   );
 };
