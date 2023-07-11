a0839008ed97d3746e90407944246297b1bd1cf5
Andres Lora <jalc.andres.lora@gmail.com>
2023-07-11 16:42:37 -0700

    created order and seller actions, reducer, store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,17 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cartReducer } from "./reducers/cart";
 import { eventReducer } from "./reducers/event";
+import { orderReducer } from "./reducers/order";
 import { productReducer } from "./reducers/product";
+import { sellerReducer } from "./reducers/seller";
 import { userReducer } from "./reducers/user";
 import { wishlistReducer } from "./reducers/wishlist";
 
 const Store = configureStore({
   reducer: {
     user: userReducer,
+    seller: sellerReducer,
     events: eventReducer,
     cart: cartReducer,
     products: productReducer,
     wishlist: wishlistReducer,
+    order: orderReducer,
   },
 });
 
