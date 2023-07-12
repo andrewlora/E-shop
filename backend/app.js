911d0dbca3050381b5b6d7f10c535bc706c1722a
Andres Lora <jalc.andres.lora@gmail.com>
2023-07-12 10:02:13 -0700

    created order and shop model, controller files

--- backend/app.js
+++ backend/app.js
@@ -29,10 +29,14 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 
 // import routes
 const user = require("./controller/user");
+const shop = require("./controller/shop");
 const product = require("./controller/product");
 const event = require("./controller/event");
+const order = require("./controller/order");
 
 app.use("/api/v2/user", user);
+app.use("/api/v2/order", order);
+app.use("/api/v2/shop", shop);
 app.use("/api/v2/product", product);
 app.use("/api/v2/event", event);
 
